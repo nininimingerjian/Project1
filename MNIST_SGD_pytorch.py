9b2a626f81035a6e1ac40a6b56d8f6f558769cb8
nininimingerjian <2964490274@qq.com>
2022-08-28 11:45:34 +0800

    更新

--- MNIST_SGD_pytorch.py
+++ MNIST_SGD_pytorch.py
@@ -15,14 +15,26 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.n1 = nn.Linear(784, 10)  # 28*28,10个分类
-        self.softmax = nn.Softmax(dim=1)
+        # self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         # 【64，1，28，28】 -》 【64，784】
         x = x.view(x.size()[0], -1)
         x = self.n1(x)
-        x = self.softmax(x)
+        # x = self.softmax(x)
         return x
+
+
+class MyDataSet():
+    def __init__(self, dataset, i_iter):
+        self.data = dataset
+        self.i_iter = torch.LongTensor(i_iter)
+
+    def __iter__(self):
+        return iter(self.i_iter)
+
+    def __len__(self):
+        return len(self.i_iter)
 
 
 model = Net()
@@ -43,18 +55,31 @@ def test_model():
 
 
 if __name__ == '__main__':
-    train_dataset = datasets.MNIST(root='./data/06_MNIST', train=True, transform=transforms.ToTensor(),
+    train_dataset = datasets.MNIST(root='./data/MNIST', train=True, transform=transforms.ToTensor(),
                                    download=True)
-    test_dataset = datasets.MNIST(root='./data/06_MNIST', train=False, transform=transforms.ToTensor(),
+    test_dataset = datasets.MNIST(root='./data/MNIST', train=False, transform=transforms.ToTensor(),
                                   download=True)
 
     n_samples = train_dataset.data.shape[0]
     batch_size = 64
-
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,
-                              shuffle=True)
+    k = np.random.randint(0, n_samples, n_samples)
+    sampler = MyDataSet(train_dataset, k)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=sampler,
+                              shuffle=False)
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,
                              shuffle=True)
+
+    features, labels = next(iter(train_loader))
+    features = Variable(features.view(features.size(0), -1))
+    labels = Variable(labels)
+    out_ = model(features)
+    loss_ = loss(out_, labels)
+    loss_.backward()
+    pre_grad = []
+    param_mean = copy.deepcopy(model)
+    for p1, p2 in zip(model.parameters(), param_mean.parameters()):
+        p2.data = p1.grad.data
+    pre_grad = [param_mean for i in range(n_samples)]
 
     # train_x, train_y = next(iter(train_loader))
     # img = torchvision.utils.make_grid(train_x, nrow=10)
@@ -62,47 +87,32 @@ if __name__ == '__main__':
     # plt.imshow(img)
     # plt.show()
 
-    num_epochs = 1
-    param_mean = copy.deepcopy(model)
     loss_list = []
-    pre_grad = [copy.deepcopy(model) for i in range(n_samples)]
-    for epoch in range(num_epochs):
 
-        for i_train, data in enumerate(train_loader):
-            inputs, labels = data
-            img = Variable(inputs.view(inputs.size(0), -1))
-            labels = Variable(labels)
+    for i_train, data in enumerate(train_loader):
+        inputs, labels = data
+        img = Variable(inputs.view(inputs.size(0), -1))
+        labels = Variable(labels)
 
-            out = model(img)
-            # labels = labels.reshape(-1, 1)
-            # one_hot = torch.zeros(inputs.shape[0], 10).scatter(1, labels, 1)
+        out = model(img)
+        # labels = labels.reshape(-1, 1)
+        # one_hot = torch.zeros(inputs.shape[0], 10).scatter(1, labels, 1)
 
-            loss1 = loss(out, labels)
-            loss_list.append(loss1.data)
+        loss1 = loss(out, labels)
+        loss_list.append(loss1.data)
 
-            loss1.backward()
-            grad_list = []
+        loss1.backward()
 
-            # for param in model.parameters():  # 784*10+10=7850
-            #     grad_list.append(param.grad)
-            #
-            # for i, param in enumerate(model.parameters()):
-            #     param.data -= grad_list[i] * lr
+        for param, param_m, pre_gr in zip(model.parameters(), param_mean.parameters(),
+                                          pre_grad[k[i_train+1]].parameters()):
+            update = (param.grad.data - pre_gr.data) + param_m.data
+            param_m.data += (param.grad.data - pre_gr.data) * (1.0 / n_samples)
+            pre_gr.data = param.grad.data
+            param.data.sub_(lr * update)
 
-            # print(param_mean)
-            # for param_m in param_mean.parameters():
-            #     print(param_m)
+        if i_train % 100 == 0:
+            print('i:', i_train)
+            test_model()
 
-            for param, param_m, pre_gr in zip(model.parameters(), param_mean.parameters(),
-                                              pre_grad[i_train].parameters()):
-                update = (param.grad.data - pre_gr.data) + param_m.data
-                param_m.data += (param.grad.data - pre_gr.data) / n_samples
-                pre_gr.data = param.grad.data
-                param.data.sub_(lr * update)
-
-            if i_train % 10 == 0:
-                print('i:', i_train)
-                test_model()
-
-        plt.plot(loss_list, 'g-')
-        plt.show()
+    plt.plot(loss_list, 'g-')
+    plt.show()
